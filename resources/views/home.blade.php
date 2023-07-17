3fee9c1de473c8cbe3be4e3455adc94bdbd17aaf
janusonyte <janusonytegabriele@gmail.com>
2023-07-17 14:49:10 +0100

    stats fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,10 +19,10 @@
                     <h3>Number of accounts in the bank: <b>{{$accounts->count()}} @for($i = 0; $i < $accounts->count(); $i++)
                         &#x1F4B0;
                     @endfor</b></h3>
-                    <h3>Amount of money stored in the bank: <b>{{$accounts->sum('balance')}}€ &#128182;</b></h3>
-                    <h3>Biggest amount held in an account: <b>{{$accounts->max('balance')}}€ &#128200;</b></h3>
-                    <h3>Smallest amount held in an account: <b>{{$accounts->min('balance')}}€ &#128201;</b></h3>
-                    <h3>Average amount held in an account: <b>{{round($accounts->avg('balance'),2)}}€ &#128184;</b></h3>
+                    <h3>Amount of money stored in the bank: <b>{{number_format($accounts->sum('balance'))}} € &#128182;</b></h3>
+                    <h3>Biggest amount held in an account: <b>{{number_format($accounts->max('balance'))}} € &#128200;</b></h3>
+                    <h3>Smallest amount held in an account: <b>{{number_format($accounts->min('balance'))}} € &#128201;</b></h3>
+                    <h3>Average amount held in an account: <b>{{number_format(round($accounts->avg('balance'),2))}} € &#128184;</b></h3>
                     <h3>Number of accounts with 0 balance: <b>{{$accounts->where('balance', 0)->count()}} &#128123;</b></h3>
                     <h3>Number of accounts with negative balance: <b>{{$accounts->where('balance','<', 0)->count()}} @if($accounts->where('balance','<', 0)->count() == 0 ) &#127881; @else &#128546; @endif</b></h3>
                 </div>
